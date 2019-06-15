7bef1c2e4266b8686a4643fd1da37cb0671b0637
grinyahoo <grinyahoo.f@gmail.com>
2019-06-14 22:36:04 -0400

    Work hours layout fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default class Footer extends React.Component {
   render() {
     return (
-      <footer className="page-footer">
+      <footer className="page-footer" id="location">
         <div className="container">
           <div className="row">
-            <div className="col l3 s12">
+            <div className="col l3 m6 s12">
               <h5 className="white-text">Contacts:</h5>
               <p className="grey-text text-lighten-4">Phone: (857) 250-5201</p>
               <p className="grey-text text-lighten-4">
@@ -16,43 +16,53 @@ export default class Footer extends React.Component {
                 E-mail: hybridcarsma@gmail.com
               </p>
             </div>
-            <div className="col l3 s12">
+            <div className="col l3 m6 s12">
               <h5 className="white-text">Hours:</h5>
               <ul>
                 <li>
-                  <div className="grey-text text-lighten-3">
-                    Mon: 9:30 am - 5:30 pm
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Mon:</div>
+                    <div className="col s10 offset-s1">9:30 am - 5:30 pm</div>
                   </div>
                 </li>
                 <li>
-                  <div className="grey-text text-lighten-3">
-                    Tue: 9:30 am - 5:30 pm
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Tue:</div>
+                    <div className="col s10 offset-s1">9:30 am - 5:30 pm</div>
                   </div>
                 </li>
                 <li>
-                  <div className="grey-text text-lighten-3">
-                    Wed: 9:30 am - 5:30 pm
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Wed:</div>
+                    <div className="col s10 offset-s1">9:30 am - 5:30 pm</div>
                   </div>
                 </li>
                 <li>
-                  <div className="grey-text text-lighten-3">
-                    Thu: 9:30 am - 5:30 pm
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Thu:</div>
+                    <div className="col s10 offset-s1">9:30 am - 5:30 pm</div>
                   </div>
                 </li>
                 <li>
-                  <div className="grey-text text-lighten-3">
-                    Fri: 9:30 am - 5:30 pm
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Fri:</div>
+                    <div className="col s10 offset-s1">9:30 am - 5:30 pm</div>
                   </div>
                 </li>
                 <li>
-                  <div className="grey-text text-lighten-3">Sat: call</div>
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Sat:</div>
+                    <div className="col s10 offset-s1">call</div>
+                  </div>
                 </li>
                 <li>
-                  <div className="grey-text text-lighten-3">Sun: closed</div>
+                  <div className="row m-0 grey-text text-lighten-3">
+                    <div className="col s1">Sun:</div>
+                    <div className="col s10 offset-s1">closed</div></div>
                 </li>
               </ul>
             </div>
-            <div className="col l6 s12" id="location">
+            <div className="col l6 s12">
               <img className="map-image-footer" width="100%" alt="Map with directions" src="/assets/images/map.png" />
             </div>
           </div>
